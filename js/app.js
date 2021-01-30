5aa616f14c454df0b6533880f9cff09858711e1a
unknown <shizusenpai97@gmail.com>
2021-01-30 18:36:38 -0300

    terminado el header sin imagenes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     effect: 'fade',
+    loop:true,
+    speed: 1000,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
