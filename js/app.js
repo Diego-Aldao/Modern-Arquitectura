efb53bec5c0cc94667b0a5189f15475e1746e189
unknown <shizusenpai97@gmail.com>
2021-02-04 19:54:36 -0300

    seccion clientes 100% responsive

--- js/app.js
+++ js/app.js
@@ -70,5 +70,12 @@ var swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
+    breakpoints:{
+      520:{
+        slidesPerView: 3,
+      },
+      768:{
+        slidesPerView: 4,
+      },
+    }
   });
